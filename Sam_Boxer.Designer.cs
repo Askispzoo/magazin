d4449d97fd2ec24f830d9ce143e1497414cd5a1f
Kacper Dudkowski <kacper.dudkowski@aski.pl>
2023-10-17 17:25:42 +0200

    Dodano kolejne pojazdy. Wprowadzono odpowiednie kontrolki do nowych formsów.

---
+++ Sam_Boxer.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace magazin
+{
+    partial class Sam_Boxer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sam_Boxer));
+            label14 = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            label11 = new Label();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(237, 886);
+            label14.Name = "label14";
+            label14.Size = new Size(123, 41);
+            label14.TabIndex = 74;
+            label14.Text = "1510 kg";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(144, 707);
+            label13.Name = "label13";
+            label13.Size = new Size(291, 41);
+            label13.TabIndex = 73;
+            label13.Text = "1500cm3, typ: Diesel";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(194, 768);
+            label12.Name = "label12";
+            label12.Size = new Size(203, 41);
+            label12.TabIndex = 72;
+            label12.Text = "ASKI sp. z.o.o.";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(264, 827);
+            label11.Name = "label11";
+            label11.Size = new Size(153, 41);
+            label11.TabIndex = 71;
+            label11.Text = "CW 62160";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = Color.Lime;
+            label10.Location = new Point(1134, 845);
+            label10.Name = "label10";
+            label10.Size = new Size(156, 62);
+            label10.TabIndex = 70;
+            label10.Text = "40 dni";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(1134, 783);
+            label9.Name = "label9";
+            label9.Size = new Size(711, 62);
+            label9.TabIndex = 69;
+            label9.Text = "Następne badanie techniczne za:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(1144, 707);
+            label8.Name = "label8";
+            label8.Size = new Size(262, 41);
+            label8.TabIndex = 68;
+            label8.Text = "Kacper Dudkowski";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(1134, 621);
+            label7.Name = "label7";
+            label7.Size = new Size(432, 62);
+            label7.TabIndex = 67;
+            label7.Text = "Ostatni użytkownik:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(21, 827);
+            label6.Name = "label6";
+            label6.Size = new Size(237, 41);
+            label6.TabIndex = 66;
+            label6.Text = "Nr. rejestracyjny:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(21, 886);
+            label5.Name = "label5";
+            label5.Size = new Size(210, 41);
+            label5.TabIndex = 65;
+            label5.Text = "Masa pojazdu:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(21, 768);
+            label4.Name = "label4";
+            label4.Size = new Size(154, 41);
+            label4.TabIndex = 64;
+            label4.Text = "Właściciel:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(21, 707);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 41);
+            label3.TabIndex = 63;
+            label3.Text = "Silnik:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(21, 621);
+            label2.Name = "label2";
+            label2.Size = new Size(326, 62);
+            label2.TabIndex = 62;
+            label2.Text = "Dane pojazdu:";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(21, 131);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1847, 471);
+            pictureBox1.TabIndex = 61;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.1F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(21, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(587, 62);
+            label1.TabIndex = 60;
+            label1.Text = "Peugeot Boxer - CW 72456";
+            // 
+            // Sam_Boxer
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1888, 992);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Sam_Boxer";
+            Text = "Peugeot Boxer";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
--- Samochody.Designer.cs
+++ Samochody.Designer.cs
@@ -32,17 +32,19 @@
             button1 = new Button();
             button2 = new Button();
             button3 = new Button();
-            tabControlAdv1 = new Syncfusion.Windows.Forms.Tools.TabControlAdv();
-            tabPageAdv1 = new Syncfusion.Windows.Forms.Tools.TabPageAdv();
             button7 = new Button();
             button6 = new Button();
             button5 = new Button();
             button4 = new Button();
-            tabPageAdv2 = new Syncfusion.Windows.Forms.Tools.TabPageAdv();
-            tabPageAdv3 = new Syncfusion.Windows.Forms.Tools.TabPageAdv();
-            ((System.ComponentModel.ISupportInitialize)tabControlAdv1).BeginInit();
-            tabControlAdv1.SuspendLayout();
-            tabPageAdv1.SuspendLayout();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            button8 = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -79,39 +81,11 @@
             button3.Text = "Usuń";
             button3.UseVisualStyleBackColor = true;
             // 
-            // tabControlAdv1
-            // 
-            tabControlAdv1.BeforeTouchSize = new Size(1704, 799);
-            tabControlAdv1.Controls.Add(tabPageAdv1);
-            tabControlAdv1.Controls.Add(tabPageAdv2);
-            tabControlAdv1.Controls.Add(tabPageAdv3);
-            tabControlAdv1.Location = new Point(21, 103);
-            tabControlAdv1.Name = "tabControlAdv1";
-            tabControlAdv1.Size = new Size(1704, 799);
-            tabControlAdv1.TabIndex = 4;
-            tabControlAdv1.SelectedIndexChanged += tabControlAdv1_SelectedIndexChanged;
-            // 
-            // tabPageAdv1
-            // 
-            tabPageAdv1.Controls.Add(button7);
-            tabPageAdv1.Controls.Add(button6);
-            tabPageAdv1.Controls.Add(button5);
-            tabPageAdv1.Controls.Add(button4);
-            tabPageAdv1.Image = null;
-            tabPageAdv1.ImageSize = new Size(40, 40);
-            tabPageAdv1.Location = new Point(1, 56);
-            tabPageAdv1.Name = "tabPageAdv1";
-            tabPageAdv1.ShowCloseButton = true;
-            tabPageAdv1.Size = new Size(1701, 741);
-            tabPageAdv1.TabIndex = 1;
-            tabPageAdv1.Text = "Osobowe";
-            tabPageAdv1.ThemesEnabled = false;
-            // 
             // button7
             // 
             button7.BackgroundImage = (Image)resources.GetObject("button7.BackgroundImage");
             button7.BackgroundImageLayout = ImageLayout.Stretch;
-            button7.Location = new Point(15, 223);
+            button7.Location = new Point(460, 6);
             button7.Name = "button7";
             button7.Size = new Size(457, 193);
             button7.TabIndex = 3;
@@ -122,7 +96,7 @@
             // 
             button6.BackgroundImage = (Image)resources.GetObject("button6.BackgroundImage");
             button6.BackgroundImageLayout = ImageLayout.Stretch;
-            button6.Location = new Point(952, 15);
+            button6.Location = new Point(6, 6);
             button6.Name = "button6";
             button6.Size = new Size(448, 193);
             button6.TabIndex = 2;
@@ -133,7 +107,7 @@
             // 
             button5.BackgroundImage = (Image)resources.GetObject("button5.BackgroundImage");
             button5.BackgroundImageLayout = ImageLayout.Stretch;
-            button5.Location = new Point(487, 15);
+            button5.Location = new Point(469, 6);
             button5.Name = "button5";
             button5.Size = new Size(448, 193);
             button5.TabIndex = 1;
@@ -144,43 +118,74 @@
             // 
             button4.BackgroundImage = (Image)resources.GetObject("button4.BackgroundImage");
             button4.BackgroundImageLayout = ImageLayout.Stretch;
-            button4.Location = new Point(15, 15);
+            button4.Location = new Point(6, 6);
             button4.Name = "button4";
             button4.Size = new Size(457, 193);
             button4.TabIndex = 0;
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
-            // tabPageAdv2
+            // tabControl1
             // 
-            tabPageAdv2.Image = null;
-            tabPageAdv2.ImageSize = new Size(40, 40);
-            tabPageAdv2.Location = new Point(1, 56);
-            tabPageAdv2.Name = "tabPageAdv2";
-            tabPageAdv2.ShowCloseButton = true;
-            tabPageAdv2.Size = new Size(1701, 741);
-            tabPageAdv2.TabIndex = 2;
-            tabPageAdv2.Text = "Małe dostawcze";
-            tabPageAdv2.ThemesEnabled = false;
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(21, 205);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1704, 697);
+            tabControl1.TabIndex = 4;
             // 
-            // tabPageAdv3
+            // tabPage1
             // 
-            tabPageAdv3.Image = null;
-            tabPageAdv3.ImageSize = new Size(40, 40);
-            tabPageAdv3.Location = new Point(1, 56);
-            tabPageAdv3.Name = "tabPageAdv3";
-            tabPageAdv3.ShowCloseButton = true;
-            tabPageAdv3.Size = new Size(1701, 741);
-            tabPageAdv3.TabIndex = 3;
-            tabPageAdv3.Text = "Dostawcze";
-            tabPageAdv3.ThemesEnabled = false;
+            tabPage1.Controls.Add(button5);
+            tabPage1.Controls.Add(button4);
+            tabPage1.Location = new Point(10, 58);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1684, 629);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Osobowe";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button7);
+            tabPage2.Controls.Add(button6);
+            tabPage2.Location = new Point(10, 58);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1684, 629);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Małe dostawcze";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(button8);
+            tabPage3.Location = new Point(10, 58);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1684, 629);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Dostawcze";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.BackgroundImage = (Image)resources.GetObject("button8.BackgroundImage");
+            button8.BackgroundImageLayout = ImageLayout.Stretch;
+            button8.Location = new Point(3, 3);
+            button8.Name = "button8";
+            button8.Size = new Size(448, 193);
+            button8.TabIndex = 4;
+            button8.UseVisualStyleBackColor = true;
             // 
             // Samochody
             // 
             AutoScaleDimensions = new SizeF(17F, 41F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1737, 914);
-            Controls.Add(tabControlAdv1);
+            Controls.Add(tabControl1);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -188,9 +193,10 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Samochody";
             Load += Samochody_Load;
-            ((System.ComponentModel.ISupportInitialize)tabControlAdv1).EndInit();
-            tabControlAdv1.ResumeLayout(false);
-            tabPageAdv1.ResumeLayout(false);
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -198,13 +204,14 @@
         private Button button1;
         private Button button2;
         private Button button3;
-        private Syncfusion.Windows.Forms.Tools.TabControlAdv tabControlAdv1;
-        private Syncfusion.Windows.Forms.Tools.TabPageAdv tabPageAdv1;
-        private Syncfusion.Windows.Forms.Tools.TabPageAdv tabPageAdv2;
-        private Syncfusion.Windows.Forms.Tools.TabPageAdv tabPageAdv3;
         private Button button5;
         private Button button4;
         private Button button7;
         private Button button6;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private Button button8;
     }
 }
